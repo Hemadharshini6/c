0570aa13b18ae025715b7250fb79bc147046bee1
Hemadharshini6 <167295786+Hemadharshini6@users.noreply.github.com>
2026-02-20 09:30:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (16.30%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+
+bool isValid(char* s) {
+    char stack[10000];
+    int top = -1;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            stack[++top] = s[i];   // push
+
+        else {
+            if (top == -1) 
+                return false;
+
+            if ((s[i] == ')' && stack[top] != '(') ||
+                (s[i] == '}' && stack[top] != '{') ||
+                (s[i] == ']' && stack[top] != '['))
+                return false;
+
+            top--;  // pop
+        }
+    }
+
+    return top == -1;
+} 
